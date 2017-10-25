d2e4b651d1c84a559db1809e593cbc2a7328ff5a
SHIN_NARITA <triplfjump1417@gmail.com>
2017-10-26 01:33:11 +0900

    2017-10-25 日報

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,21 +3,77 @@ $(function() {
   var $fontSizeLarge = $('.js-large');
 
 
+  // var fontsize = $('.js-fs').css('font-size');
+  // console.log(fontsize);
+
+
+
   $fontSizeDefault.on({
     'mouseenter': function() {
-      $fontSizeDefault.attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_default_on.png');
+      $(this).attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_default_on.png');
     },
     'mouseleave': function() {
-      $fontSizeDefault.attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_default.png');
+      $(this).attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_default.png');
     }
   });
 
   $fontSizeLarge.on({
     'mouseenter': function() {
-      $fontSizeLarge.attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_large_on.png');
+      $(this).attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_large_on.png');
     },
     'mouseleave': function() {
-      $fontSizeLarge.attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_large.png');
+      $(this).attr('src', 'http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/head/btn_txt_large.png');
     }
   });
+
+  // $fontSizeDefault.on('click', function(event) {
+  //   $('.js-fs').addClass('is-default');
+  // });
+
+  // $fontSizeLarge.on('click', function(event) {
+  //   var up = fontsize * '150%';
+  //   console.log(up);
+  //   $('.js-fs').addClass('is-large');
+  //   $('.js-fs').css('font-size', up);
+  // });
+
+  $('.js-menu').hover(function() {
+    $(this).children('ul').fadeIn(400);
+    // .addClass('is-active');
+  }, function() {
+    $(this).children('ul').fadeOut(0);
+    // .removeClass('is-active');
+  });
+
+  $.ajax({
+    url: '../../api/facilities.json',
+    dataType: 'json',
+  })
+  .done(function(data) {
+    showFacilities(data);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+  function showFacilities(data) {
+    data.facilities.forEach(function(facilities) {
+      var items =
+        '<li>'+
+          '<figure class="p-facility__item">'+
+            '<a href="">'+
+              '<img src="'+ facilities.img +'" alt="" class="p-facility__img">'+
+            '</a>'+
+          '</figure>'+
+          '<a href="" class="p-facility__link">'+ facilities.name +'<img src="http://www.greenlife-inc.co.jp/wp-content/themes/greenlife-inc/images/top/btn_link.png" alt="" class="p-facility__link__btn">'+
+          '</a>'+
+          '<p class="p-facility__category">'+ facilities.category +'</p>'+
+        '</li>';
+
+      $('.p-facility__list').append(items);
+    });
+  }
 });
